2aa5070ac56098558d1a4ca9024a4230c6eac453
makinenvilma <vilmakinen@outlook.com>
2025-05-21 14:38:31 +0300

    Add files

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-kz9kstl=i$cu8d5gbqjcgo*u()az&$90p%1n)mej4f+*c6!u)$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
